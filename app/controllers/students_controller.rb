51137dc467b824e84057a4d4f7ccb8f5c5b87a0a
github username <github email address>
2018-11-04 11:51:55 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -7,5 +7,16 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+
+  def new
+
+  end
+
+
+  def create
+    Student.create(params[:id])
+  end
+
 
 end
